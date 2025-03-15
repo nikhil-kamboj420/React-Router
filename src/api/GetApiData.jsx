bafa9d0ac35a9989a749d9607ea359e5a36eb075
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-03-15 12:53:32 +0530

    configure .env files

--- src/api/GetApiData.jsx
+++ src/api/GetApiData.jsx
@@ -1,6 +1,6 @@
 export const getMoviesData = async () => {
     try{
-       const  response =  await fetch('https://www.omdbapi.com/?i=t3896198&apikey=a8dfacb3&s=titanic&page=1');
+       const  response =  await fetch(`https://www.omdbapi.com/?i=t3896198&apikey=${import.meta.env.VITE_Movie_API_KEY}&s=titanic&page=1`);
        const data = await response.json();
        return data;
 
