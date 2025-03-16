7a9f3a77221052a69f3c8799da88fd817fd0c64e
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-03-16 16:22:19 +0530

    add Form component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Movie } from './pages/Movie';
-import { Contact } from './pages/Contact';
+import { Contact, contactFormData } from './pages/Contact';
 import { AppLayout } from './components/layout/AppLayout';
 import { ErrorPage } from './pages/ErrorPage';
 import { getMoviesData } from './api/GetApiData';
@@ -37,6 +37,7 @@ const App = () => {
         {
           path: '/contact',
           element: <Contact />,
+          action:contactFormData,
         },
       ],
     },
