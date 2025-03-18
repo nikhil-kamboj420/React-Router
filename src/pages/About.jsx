12c0a166b984f1e94e9e0a8ee28642f232a37f78
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-03-18 21:44:37 +0530

    add about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,14 @@
 
 
-export const About = ()=>{
-    return <h1>About</h1>
-}
+import React from 'react';
+
+export const About = () => {
+    return (
+        <div className=" about-bg flex flex-col items-center gap-7 justify-center min-h-screen p-4 text-white">
+            <h1 className="text-8xl font-bolder text-center mb-4 animate-fade-in">About Our Movie Project</h1>
+            <p className="text-5xl text-center mb-4 max-w-2xl">
+                This movie project is a thrilling adventure that takes you through the journey of...
+            </p>
+        </div>
+    );
+};
